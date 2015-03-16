a90f6db9afbcc004ece939309282ab3bd5f44512
Marissa Schmidt <marissaschmidt@boisestate.edu>
2015-03-16 14:08:29 -0600

    Getting rid of seg fault

--- C-examples/file-io/filesize.c
+++ C-examples/file-io/filesize.c
@@ -18,12 +18,13 @@ int main(int argc, char **argv)
 	fin = fopen(argv[1], "r");
 	if (!fin) {
 		perror("filesize:");
+		exit(-1);
 	}
 	fseek(fin, 0, SEEK_END);
 	size = ftell(fin);
 	printf("Size of the file %s = %lf MB\n", argv[1],  (double) size/(1024*1024));
 
-	// Alternate method. Not fully portable. POSIX standard, not C standard.	
+	// Alternate method. Not fully portable. POSIX standard, not C standard.
 	/*struct stat st;
 	stat(argv[1], &st);
 	size = st.st_size;
