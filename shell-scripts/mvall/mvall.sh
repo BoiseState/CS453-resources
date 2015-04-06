9708aa0ea51c6df5aa3b67ac714a43a2bec3450d
Marissa Schmidt <marissaschmidt@boisestate.edu>
2015-04-06 10:23:02 -0600

    Minor updates to example.

--- shell-scripts/mvall/mvall.sh
+++ shell-scripts/mvall/mvall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 case $# in
-0|1) echo "Usage " $(basename $0) " <oldext> <newext>"; exit 1;;
+0|1) echo "Usage " $(basename $0) " <oldext> <newext>"; exit 1;
 esac
 
 for f in *.$1
 do
   echo $f
-  base=$(basename $f .$1)
+  base=$(basename $f .$1) # e.g. f1.cpp => f1
   mv $f $base.$2
 done
