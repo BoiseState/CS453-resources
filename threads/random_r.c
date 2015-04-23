f0a68ec7e08a12c84f844e58a8ea3a0a614f5ba8
Shane Panter <shane@foundationcode.com>
2015-04-22 18:52:56 -0600

    Updated random example

--- threads/random_r.c
+++ threads/random_r.c
@@ -4,27 +4,26 @@
 #include <pthread.h>
 #define STATE_LEN 8
 
-void* random_printer(void *ptr); 
+void* random_printer(void *ptr);
 int  main()
 {
      pthread_t thread1, thread2;
-     int s1 = 1;
-     int s2 = 2;
-     pthread_create( &thread1, NULL, random_printer,(void*)&s1); 
-     pthread_create(&thread2, NULL, random_printer, (void*)&s2);
-     pthread_join(thread1, NULL); 
+     pthread_create(&thread1, NULL, random_printer,(void*)1);
+     pthread_create(&thread2, NULL, random_printer, (void*)2);
+     pthread_join(thread1, NULL);
      pthread_join(thread2, NULL);
      exit(0);
 }
+
 void* random_printer(void *ptr)
 {
     //get the seed that was passed into the thread
-    int seed = *((int*)ptr);
+    int seed = (size_t)ptr;
     //create our random_data buff on the stack
     struct random_data buff = {};
     //create a place to store the result from random_r on the stack
     int32_t result =0;
-    //create our statebuff for initState_r. Note: that statebuff must not be less than 8 bytes  
+    //create our statebuff for initState_r. Note: that statebuff must not be less than 8 bytes
     char statebuf[STATE_LEN];
     //init our state for random_r
     initstate_r(seed,statebuf,STATE_LEN,&buff);
@@ -33,7 +32,7 @@ void* random_printer(void *ptr)
     for(i=0;i<10000;i++ ){
         //get our random numbers.
         random_r(&buff,&result);
-        /* printf("%d\n",result); */
+        printf("%d\n",result);
     }
     pthread_exit(NULL);
 }
