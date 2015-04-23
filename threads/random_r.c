5399af3248208fbf51411d8f431896fc6245f621
Shane Panter <shane@foundationcode.com>
2015-04-22 18:38:04 -0600

    Updated random_r.c to use the stack instead of the heap

--- threads/random_r.c
+++ threads/random_r.c
@@ -1,28 +1,39 @@
-#include <errno.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#define STATE_LEN 8
 
-#define BUFSIZE 32
-
-const int SEED = 1234;
-
-int main(int argc, char **argv)
+void* random_printer(void *ptr); 
+int  main()
 {
-    struct random_data *rdata = malloc(sizeof(struct random_data));
-    char *statebuf = calloc(BUFSIZE, sizeof(char));
-
-    int32_t value;
-
-    initstate_r(SEED, statebuf, BUFSIZE, rdata);
-    int status = random_r (rdata, &value);
-    if(status != 0)
-    {
-        perror("random_r");
-        exit(status);
+     pthread_t thread1, thread2;
+     int s1 = 1;
+     int s2 = 2;
+     pthread_create( &thread1, NULL, random_printer,(void*)&s1); 
+     pthread_create(&thread2, NULL, random_printer, (void*)&s2);
+     pthread_join(thread1, NULL); 
+     pthread_join(thread2, NULL);
+     exit(0);
+}
+void* random_printer(void *ptr)
+{
+    //get the seed that was passed into the thread
+    int seed = *((int*)ptr);
+    //create our random_data buff on the stack
+    struct random_data buff = {};
+    //create a place to store the result from random_r on the stack
+    int32_t result =0;
+    //create our statebuff for initState_r. Note: that statebuff must not be less than 8 bytes  
+    char statebuf[STATE_LEN];
+    //init our state for random_r
+    initstate_r(seed,statebuf,STATE_LEN,&buff);
+    printf("%p\n",&buff);
+    int i;
+    for(i=0;i<10000;i++ ){
+        //get our random numbers.
+        random_r(&buff,&result);
+        /* printf("%d\n",result); */
     }
-
-    printf("%d\n", value);
-    exit(0);
+    pthread_exit(NULL);
 }
