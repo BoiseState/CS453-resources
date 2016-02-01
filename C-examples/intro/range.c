d2a40e335ca5e4400a92344c357531abccfc05eb
Marissa R. Schmidt <marissaschmidt@boisestate.edu>
2016-01-31 21:45:40 -0700

    Fixing line endings

--- C-examples/intro/range.c
+++ C-examples/intro/range.c
@@ -1,21 +1,23 @@
-
+/* Run
+ *   gcc -M range.c
+ * to locate limits.h and float.h headers
+ */
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
-
 
 int main ()
 {
     printf("%25s %25s %25s\n", "Type", "Min", "Max");
 
     printf("%25s %25d %25d\n", "char", CHAR_MIN, CHAR_MAX);
-    printf("%25s %25u %25u\n", "unsigned char", 0, UCHAR_MAX);
+    printf("%25s %25u %25u\n", "unsigned char", 0U, UCHAR_MAX);
 
     printf("%25s %25d %25d\n", "short", SHRT_MIN, SHRT_MAX);
-    printf("%25s %25u %25u\n", "unsigned short",0, USHRT_MAX);
+    printf("%25s %25u %25u\n", "unsigned short", 0U, USHRT_MAX);
 
     printf("%25s %25d %25d\n", "int", INT_MIN, INT_MAX);
-    printf("%25s %25u %25u\n", "unsigned int", 0, UINT_MAX);
+    printf("%25s %25u %25u\n", "unsigned int", 0U, UINT_MAX);
 
     printf("%25s %25ld %25ld\n", "long", LONG_MIN, LONG_MAX);
     printf("%25s %25lu %25lu\n", "unsigned long", 0UL, ULONG_MAX);
