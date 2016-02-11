12ff5634b4d081d06aaef6f9fbb8476fa0cf86be
Marissa Schmidt <marissaschmidt@boisestate.edu>
2016-02-11 13:48:41 -0700

    format.sh

--- format.sh
+++ format.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# To use:
+# =====================================================================
+# 1. Copy this file into your project directory.
+# 2. Change file permissions so this is an executable.
+#    chmod +x format.sh
+# 3. Execute this script in your project directory to format all files
+#    ./format.sh
+
 astyle --style=kr --indent=spaces --convert-tabs --recursive "./*.cpp"
-astyle --style=kr --indent=spaces --convert-tabs --recursive "./*.h" 
-astyle --style=kr --indent=spaces --convert-tabs --recursive "./*.c" 
+astyle --style=kr --indent=spaces --convert-tabs --recursive "./*.h"
+astyle --style=kr --indent=spaces --convert-tabs --recursive "./*.c"
