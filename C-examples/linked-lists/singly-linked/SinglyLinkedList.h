bc573a5b2a7399d07701c03887df19e48568c21d
boiseamit <ajain@boisestate.edu>
2016-07-22 16:11:39 -0600

    Improve the Singly Linked List example

--- C-examples/linked-lists/singly-linked/SinglyLinkedList.h
+++ C-examples/linked-lists/singly-linked/SinglyLinkedList.h
@@ -4,6 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* 
+ * The overall list structure
+ */
+struct list {
+	int size;
+	struct node *head;
+};
+
+
 /*
  * Linked list node
  */
@@ -13,12 +23,19 @@ struct node {
 };
 
 /**
+  Create an empty list.
+
+  @return pointer to the list structure
+ */
+struct list *createList();
+
+/**
   Add a node to the front  of the list.
   @param list pointer to front of the list
   @param node the new node to add
   @return the current pointer to the front of the list
  */
-struct node* addAtFront(struct node *head, struct node *node);
+struct list *addAtFront(struct list *list, struct node *node);
 
 
 /**
@@ -27,13 +44,13 @@ struct node* addAtFront(struct node *head, struct node *node);
   @param list pointer to front of the list
   @return pointer to the front of the reversed list
  */
-struct node* reverseList(struct node *head);
+struct list *reverseList(struct list *list);
+
 
 /**
   Print the list from start to end.
   @param list pointer to front of the list
 */
-void printList(struct node *head);
-
+void printList(struct list *list);
 
 #endif /* __SINGLYLINKEDLIST_H */
