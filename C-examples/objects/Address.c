f987a55736637c8b266c50c6a3540b839871ef3d
amit <ajain@boisestate.edu>
2016-07-25 15:05:10 -0600

    Add constants instead of magic numbers in Address.c

--- C-examples/objects/Address.c
+++ C-examples/objects/Address.c
@@ -3,20 +3,22 @@
 
 #include "Address.h"
 
+#define PADDING (4+5+10)
+
 static int getLength(Address this)
 {
-    return strlen(this->name)+strlen(this->streetAddress)+
-           strlen(this->city)+strlen(this->state)+4+5+10;
+    return strlen(this->name) + strlen(this->streetAddress) + 
+           strlen(this->city) + strlen(this->state) + PADDING;
 }
 
 Address createAddress(char *name, char *streetAddress, char *city,
                       char *state, int zip, char *(*printAddress)(struct address *))
 {
     Address temp = (Address) malloc(sizeof(address));
-    temp->name = (char *) malloc(sizeof(char)*(strlen(name)+1));
-    temp->streetAddress = (char *) malloc(sizeof(char)*(strlen(streetAddress)+1));
-    temp->city = (char *) malloc(sizeof(char)*(strlen(city)+1));
-    temp->state = (char *) malloc(sizeof(char)*(strlen(state)+1));
+    temp->name = (char *) malloc(sizeof(char)* (strlen(name) + 1));
+    temp->streetAddress = (char *) malloc(sizeof(char)*(strlen(streetAddress) + 1));
+    temp->city = (char *) malloc(sizeof(char) * (strlen(city) + 1));
+    temp->state = (char *) malloc(sizeof(char) * (strlen(state) + 1));
     strcpy(temp->name, name);
     strcpy(temp->streetAddress, streetAddress);
     strcpy(temp->city, city);
@@ -32,7 +34,7 @@ Address createAddress(char *name, char *streetAddress, char *city,
 char *printMultiLine(Address this)
 {
     int len = getLength(this);
-    char * temp = (char *) malloc(sizeof(char)*len);
+    char *temp = (char *) malloc(sizeof(char) * len);
     snprintf(temp, len, "%s\n%s\n%s, %s, %d\n", this->name,
              this->streetAddress, this->city, this->state, this->zip);
     return temp;
@@ -41,7 +43,7 @@ char *printMultiLine(Address this)
 char *printOneLine(Address this)
 {
     int len = getLength(this);
-    char * temp = (char *) malloc(sizeof(char)*len);
+    char *temp = (char *) malloc(sizeof(char)* len);
     snprintf(temp, len, "%s, %s, %s, %s %d\n", this->name,
              this->streetAddress, this->city, this->state, this->zip);
     return temp;
