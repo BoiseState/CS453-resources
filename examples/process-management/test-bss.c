41fa90f821055df95d7d64d0863d4801fedf3210
amit <ajain@boisestate.edu>
2016-09-13 23:51:08 -0600

    Update test-bss example

--- examples/process-management/test-bss.c
+++ examples/process-management/test-bss.c
@@ -1,6 +1,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * This program shows that a global array with all zeroes in stored
+ * in a block started by symbol (bss) section, which only stores the
+ * name of the block and number of zeroes. This reduces the size of
+ * the executable and makes it faster to load. The block is filled 
+ * with zeroes on loading.
+ */
+
 int a[100000000];
 
 int main()
