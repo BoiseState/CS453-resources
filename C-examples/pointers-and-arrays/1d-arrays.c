1c7d54d87e43877b4c02159a065ee3d4a10b4dcb
Marissa Schmidt <marissaschmidt@boisestate.edu>
2016-09-21 08:47:59 -0600

    Cleaned up 1d-arrays example. Moved swap to array-swap.c

--- C-examples/pointers-and-arrays/1d-arrays.c
+++ C-examples/pointers-and-arrays/1d-arrays.c
@@ -2,73 +2,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DEBUG
+
 /* A macro for printing array debug statements. To compile with debugging,
  * gcc -Wall -g -DDEBUG 1d-arrays.c -o 1d-arrays */
 #ifdef DEBUG
-#define debug(a, n, m) printf("\n" m "\n"); printArray((a), (n))
+#define debug(a, n, m) printf("\n\t[DEBUG] " m "\n"); print_array((a), (n))
 #else
 #define debug(a, n, m)
 #endif
 
 #define SIZE 5
 
-void printArray(int *values, int size);
-void swapArrays(int **a, int **b);
+static void print_array(int *values, int size);
 
 int main(int argc, char **argv)
 {
-    int A[SIZE]; /* static allocation */
-    int *B = (int *) malloc(sizeof(int) * SIZE); /* dynamic allocation */
-    int *C; /* C will point to an array of n int's */
-    int n = 10;
     int i;
+    
+	int digits[SIZE]; /* static allocation */
+    int *pdigits = digits; /* pointer to digits array */
 
-    debug(A, SIZE, "A before init'ing elements");
+	int n = rand() % 100; /* random array size */
+    int *zeros; /* dynamically allocated below */
+
+	printf("\nStatically declared array of %d elements...\n", SIZE);
+    debug(digits, SIZE, "digits before init'ing elements");
+    debug(pdigits, SIZE, "pdigits before init'ing digits");
+
+	/* Initialize values of digits */
+	printf("\nInitializing digits...\n");
     for (i = 0; i < SIZE; i++) {
-        A[i] = i;
+        digits[i] = i;
     }
-    debug(A, SIZE, "A after init'ing elements");
-    C = A;
-    debug(C, SIZE, "C after assigning A");
+    debug(digits, SIZE, "digits after init'ing elements");
+    debug(pdigits, SIZE, "pdigits after init'ing digits");
 
-    debug(B, SIZE, "B before init'ing elements");
-    for (i = 0; i < SIZE; i++) {
-        B[i] = i;
-    }
-    debug(B, SIZE, "B after init'ing elements");
+	/* Allocate memory for zeros array */
+	printf("\nDynamically allocating %d elements...\n", n);
+    //debug(zeros, n, "zeros before malloc'ing memory"); /* this may cause seg fault (if we are lucky) */
+    zeros = (int *) malloc(sizeof(int) * n);
+    debug(zeros, n, "zeros after malloc'ing memory");
 
-    debug(C, n, "C before malloc'ing memory"); /* this may cause seg fault (if we are lucky) */
-    C = (int *) malloc(sizeof(int) * n);
-    debug(C, n, "C before init'ing elements");
+	/* Initialize zeros array */
+    debug(zeros, n, "zeros before init'ing elements");
     for (i = 0; i < n; i++) {
-        C[i] = 1;  /* equivalent to *(C+i) */
+        zeros[i] = 0; /* equivalent to *(C+i) */
     }
-    debug(C, n, "C after init'ing elements");
+    debug(zeros, n, "zeros after init'ing elements");
 
-    swapArrays(&B, &C);
-    debug(B, n, "B after swap");
-    debug(C, SIZE, "C after swap");
+	/* Could also just use calloc if we know we want all zeros */	
+    /* zeros = (int *) calloc(sizeof(int) * n, sizeof(int)); */
 
-    C = (int *) realloc(C, sizeof(int) * (SIZE * 2));
-    for(i = SIZE; i < (SIZE * 2); i++) {
-        C[i] = i;
+	/* Doubling the size of zeros */
+	printf("\nDoubling size of zeros...\n");
+    zeros = (int *) realloc(zeros, sizeof(int) * (n * 2));
+    for(i = n; i < (n * 2); i++) {
+        zeros[i] = 0;
     }
-    debug(C, SIZE*2, "C after realloc");
+    debug(zeros, n*2, "zeros after realloc");
+    
+	/* Free memory (for every malloc there is a free) */
+	printf("\nFreeing zeros...\n");
+	free(zeros);
+    debug(zeros, n*2, "zeros after free");
 
-    /* Delete the arrays that B and C point to */
-    free(B);
-    free(C);
+	/* pdigits is also a pointer. Should I free it? Why or why not? */
 
-    debug(B, n, "B after free");
-    debug(C, SIZE*2, "C after free");
-
-    /* Always set your pointers to NULL when you are done. */
-    B = C = NULL;
+    /* Always set your pointers to NULL when you are done */
+	printf("\nPreventing \"dangling pointers\"...\n");
+    zeros = NULL;
+	printf("zeros (%p)\n", zeros);
 
     return 0;
 }
 
-void printArray(int *values, int size)
+static void print_array(int *values, int size)
 {
     int i = 0;
     printf("\tPointer: %p\n\t", values);
@@ -76,11 +85,4 @@ void printArray(int *values, int size)
         printf("[%d]", values[i]);
     }
     printf("\n");
-}
-
-void swapArrays(int **a, int **b)
-{
-    int *tmp = *a;
-    *a = *b;
-    *b = tmp;
 }
