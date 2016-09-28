d385bba7e531644c2c7a0b6f00ea4d4123c8469b
Marissa Schmidt <marissaschmidt@boisestate.edu>
2016-09-28 07:56:21 -0600

    Moving singly-linked list source files into libsrc directory

--- C-examples/linked-lists/singly-linked-better/run.sh
+++ C-examples/linked-lists/singly-linked-better/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-UnitTestList
+testsuite/UnitTestList
 # Maybe we want to have all the results in a file?
 # make sure and redirect stdout and stderr if you want both!
 # testsuite/UnitTestList >> results.txt
-SimpleTestList
+testsuite/SimpleTestList
 #Modify this Script to pass command line args to set RandomTestList
-RandomTestList 10 100
-valgrind --leak-check=full RandomTestList 10 100
+testsuite/RandomTestList 10 100
+valgrind --leak-check=full testsuite/RandomTestList 10 100
