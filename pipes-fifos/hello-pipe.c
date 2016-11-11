ee636d26c3cb577afe3ba53f749564d83c196f66
Marissa Schmidt <marissaschmidt@boisestate.edu>
2016-11-11 08:20:08 -0700

    Closing pipes when done reading/writing

--- pipes-fifos/hello-pipe.c
+++ pipes-fifos/hello-pipe.c
@@ -4,6 +4,7 @@ int main(void)
 {
     pid_t pid;
     int n, fd[2];
+    char *message;
     char line[MAXLINE];
 
     if (pipe(fd) < 0) {
@@ -22,11 +23,15 @@ int main(void)
     case 0: /* child */
         close(fd[1]); /* close write end of pipe */
         n = read(fd[0], line, MAXLINE);
-        printf("child read %d chars from the parent in the pipe: %s", n, line);
+        printf("child read %d chars from the parent in the pipe: %s\n", n, line);
+        close(fd[0]); /* close read end when we are done */
         exit(EXIT_SUCCESS);
     default: /* parent */
+        message = "hello world";
         close(fd[0]); /* close read end of pipe */
-        write(fd[1], "hello world\n", 12);
+        write(fd[1], message, strlen(message));
+        write(fd[1], " you are awesome", strlen(" you are awesome")+1 );
+        close(fd[1]); /* close write end when we are done */
         exit(EXIT_SUCCESS);
     }
 }
