5a969e4ce9c588afd09f26c2d78336516a7fcd9e
Marissa Schmidt <marissaschmidt@boisestate.edu>
2016-11-11 08:23:45 -0700

    Minor cleanup to signal examples

--- signals/timeout.c
+++ signals/timeout.c
@@ -10,52 +10,61 @@
 #include <sys/wait.h>
 #include <string.h>
 
-int pid; /* child process id */
-char *progname;
-
+/* alarm signal handler function */
 static void onalarm(int signo);
+
+/* child process id. global so we have access to it in the 
+ * signal handler. */
+static int pid;
 
 int main(int argc, char *argv[])
 {
     void error(char *msg, char *arg);
-    int sec=10; /* default timeout */
+    int timeout = 10;
     int status;
+    char *progname;
 
-    /*signal(SIGINT, SIG_IGN);*/
     progname = argv[0];
     if (argc > 1 && argv[1][0] == '-') {
-        sec = atoi(&argv[1][1]);
+        timeout = atoi(&argv[1][1]);
         argc--;
         argv++;
     }
 
     if (argc < 2) {
-        error("Usage: %s [-10] command", progname);
+        fprintf(stderr, "Usage: %s [-<timeout>] <command>\n", progname);
+        exit(EXIT_FAILURE);
     }
 
+    /* install alarm signal handler */
     signal(SIGALRM, onalarm);
-    alarm(sec);
-    if ((pid=fork()) == 0) {
-        execvp(argv[1], &argv[1]);
-        error("couldn't start %s", argv[1]);
-    }
-    if ((wait(&status) == -1) || WIFSIGNALED(status)) {
-        int signo = WTERMSIG(status);
 
-        printf("%s %s", argv[1], strsignal(signo));
+    /* set alarm for timeout seconds */
+    alarm(timeout);
+
+    /* send child on its way */
+    if ((pid = fork()) == 0) {
+        printf("child [%d] executing %s\n", getpid(), argv[1]);
+        execvp(argv[1], &argv[1]);
+        perror("exec failed");
+        exit(EXIT_FAILURE);
     }
+
+    /* wait for it to finish */
+    if((pid = wait(&status)) == -1) {
+        perror("wait");
+        exit(EXIT_FAILURE);
+    } else if(WIFEXITED(status)) {
+        printf("child [%d] exited normally with status %d\n", pid, WEXITSTATUS(status));
+    } else if(WIFSIGNALED(status)) {
+        printf("child [%d] terminated by signal %s\n", pid, strsignal(WTERMSIG(status)));
+    }
+
     exit(WEXITSTATUS(status));
 }
 
 static void onalarm(int signo)   /* kill child process when alarm arives */
 {
     kill(pid, SIGKILL);
-}
-
-void error(char *msg, char *arg)
-{
-    fprintf(stderr, msg, arg);
-    fprintf(stderr,"\n");
-    exit(EX_SOFTWARE);
 }
 
