12e7d253666d06f8346927162ec0d26886739740
Jim Buffenbarger <buff@cs.boisestate.edu>
2023-10-30 18:00:05 -0600

    update pubs: Mon Oct 30 06:00:05 PM MDT 2023

--- buff/classes/452/pub/ch26/BoxMon/Box.java
+++ buff/classes/452/pub/ch26/BoxMon/Box.java
@@ -3,19 +3,19 @@ public class Box {
     private Object contents;
 
     public synchronized Object get() {
-	while (contents==null)
-	    try { wait(); } catch (Exception e) {}
-	Object o=contents;
-	contents=null;
-	notify();
-	return o;
+        while (contents==null)
+            try { wait(); } catch (Exception e) {}
+        Object o=contents;
+        contents=null;
+        notify();
+        return o;
     }
 
     public synchronized void put(Object o) {
-	while (contents!=null)
-	    try { wait(); } catch (Exception e) {}
-	contents=o;
-	notify();
+        while (contents!=null)
+            try { wait(); } catch (Exception e) {}
+        contents=o;
+        notify();
     }
 
 }
--- buff/classes/452/pub/ch26/BoxMon/TryBox.java
+++ buff/classes/452/pub/ch26/BoxMon/TryBox.java
@@ -4,25 +4,25 @@ public class TryBox extends Thread {
     private String _msg;
 
     public TryBox(Box box, String msg) {
-	_box=box;
-	_msg=msg;
+        _box=box;
+        _msg=msg;
     }
 
     public void run() {
-	_box.put(_msg);
-	System.out.println(_box.get());
+        _box.put(_msg);
+        System.out.println(_box.get());
     }
 
     public static void main(String[] args) {
-	Box box=new Box();
-	TryBox t1=new TryBox(box,"t1");
-	TryBox t2=new TryBox(box,"t2");
-	t1.start();
-	t2.start();
-	try {
-	    t1.join();
-	    t2.join();
-	} catch (Exception e) {}
+        Box box=new Box();
+        TryBox t1=new TryBox(box,"t1");
+        TryBox t2=new TryBox(box,"t2");
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {}
     }
 
 }
