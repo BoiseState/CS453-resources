6c9ac0d82d609bb088a058afa3f54dfd4f0f4d63
Jim Buffenbarger <buff@cs.boisestate.edu>
2024-08-30 08:34:26 -0600

    update pubs: Fri Aug 30 08:34:26 MDT 2024

--- buff/classes/452/pub/hw1/error.h
+++ buff/classes/452/pub/hw1/error.h
@@ -4,6 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// We want to be able to reference these macros like function calls:
+//     if (...)
+//         ERROR(...);
+//     else
+//         ...
+// So, ERROR(...) should expand to a single C statement,
+// that requires the reference to add the semicolon.
+// If the expansion ended with a semicolon, or was {...},
+// adding a semicolon would make *two* statements,
+// which would interfere with the "else".
+
+// Since these definitions each contain multiple fprintf() calls, the
+// calls must be wrapped in a {...}, which must be wrapped in a
+// different kind of statement, which *requires* an added semicolon.
+// In C and C++, a "do" loop:
+//    do {...} while (...)
+// is the only such statement.
+// To execute the body exactly once, its test must evaluate to false.
+
 #define WARNLOC(file,line,kind,args...) do {  \
   fprintf(stderr,"%s:%d: ",file,line);        \
   fprintf(stderr,"%s: ",kind);                \
