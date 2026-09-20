8ae95e62748826d401a84668be78414dcfd04fec
Jim Buffenbarger <Jim Buffenbarger>
2026-09-19 18:00:00 -0600

    update pubs: Sat Sep 19 06:00:00 PM MDT 2026

--- buff/classes/452/pub/hw2/balloc.h
+++ buff/classes/452/pub/hw2/balloc.h
@@ -4,12 +4,12 @@
 typedef void *Balloc;
 
 extern Balloc bcreate(unsigned int size, int l, int u);
-extern void   bdelete(Balloc pool);
+extern void   bdelete(Balloc ba);
 
-extern void *balloc(Balloc pool, unsigned int size);
-extern void  bfree(Balloc pool, void *mem);
+extern void *balloc(Balloc ba, unsigned int size);
+extern void  bfree(Balloc ba, void *mem);
 
-extern unsigned int bsize(Balloc pool, void *mem);
-extern void bprint(Balloc pool);
+extern unsigned int bsize(Balloc ba, void *mem);
+extern void bprint(Balloc ba);
 
 #endif
